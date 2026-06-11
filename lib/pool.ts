aa43b91d25c5de6561e4aaa9ca6650d985cd066f
Tensorium Labs <dev@tensoriumlabs.com>
2026-06-11 09:08:57 +0000

    chore: update pool treasury address

--- lib/pool.ts
+++ lib/pool.ts
@@ -90,7 +90,7 @@ export type PoolSnapshot = {
 export const POOL_FEE_BPS = 500;
 export const POOL_FEE_PERCENT = POOL_FEE_BPS / 100;
 export const POOL_TREASURY_ADDRESS =
-  "txm13vgxzj5ulrfhe7x0mlzxg0q6veq42tkku4g3jr";
+  "txm1px2nmtp087mz8dv3lplqadwzxawk0c5kg0mt24";
 
 const EMPTY_STATS: PoolStats = {
   blocks_found: 0,
